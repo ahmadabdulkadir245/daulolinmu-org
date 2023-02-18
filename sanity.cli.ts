0c4abd4edc3a83df93d795f0d95157deb562b699
unknown <ahmadabdulkadir245@gmail.com>
2023-02-18 07:46:27 -0800

    not using environment variables

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,7 +5,7 @@ import { datasetKey, projectIdKey } from './lib/constant'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_NODE_ENV
+    projectId: projectIdKey,
+    dataset: datasetKey
   }
 })
