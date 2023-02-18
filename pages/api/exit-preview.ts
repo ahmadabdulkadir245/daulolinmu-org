bbf470f5832c7cada0d2b5a723d71ef834a1f9db
unknown <ahmadabdulkadir245@gmail.com>
2023-02-18 06:35:32 -0800

    new style

--- pages/api/exit-preview.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function exit(req: NextApiRequest, res: NextApiResponse) {
-    res.clearPreviewData({});
-    res.writeHead(307, {location: '/'})
-    res.end()
-}
