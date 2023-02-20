014a6b56b776c99ae397536c3cde80a6b9f920f6
unknown <ahmadabdulkadir245@gmail.com>
2023-02-20 07:37:05 -0800

    new update

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,11 +1,11 @@
 import {createClient} from 'next-sanity'
-
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+import { datasetKey, projectIdKey } from './constant'
+export const dataset = process.env. SANITY_PROJECT_ID
+export const projectId = process.env.SANITY_PROJECT_ID
 
  export const client = createClient({
-    projectId,
-    dataset,
+    projectId: projectIdKey,
+    dataset: datasetKey,
      apiVersion:  "2022-11-15",
     useCdn: false
  }) 
--- lib/sanity.preview.ts
+++ lib/sanity.preview.ts
@@ -3,19 +3,22 @@
 
 import {definePreview} from 'next-sanity/preview'
 import {dataset, projectId} from './sanity.client'
+import { datasetKey, projectIdKey } from './constant'
+
+
 
 function onPublicAccessOnly() {
     throw new Error('Unable to load preview as you are not logged in')
 }
 
-if(!projectId || !dataset) {
+if(!projectIdKey || !datasetKey) {
     throw new Error(
         'Missing projectId or dataset. Check your sanity.json or .env'
     );
 }
 
 export const usePreview = definePreview({
-    projectId,
-    dataset,
+    projectId: projectIdKey,
+    dataset: datasetKey,
     onPublicAccessOnly
 })
